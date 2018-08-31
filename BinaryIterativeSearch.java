268b954283c89fa55373acc1a613e1212d478216
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-08-31 14:49:57 -0700

    Finished Binary Iterative Search

--- BinaryIterativeSearch.java
+++ BinaryIterativeSearch.java
@@ -1,13 +1,22 @@
 public class BinaryIterativeSearch implements Practice2Search {
 	public String searchName() {
-		return "Iterative Search";
+	       return "Binary Iterative Search";
 	}
-	public int search(int[] arr, int target) {	
-		for (int i = 0; i < arr.length; i++) {
-			if (i == target) {
-				return i;
+	public int search(int[] arr, int target) {
+		int low = 0;
+		int top = arr.length - 1;
+
+		for (int i = 0; low < top; i++) {
+			int mid = top - low / 2;
+			if (target < arr[mid]) {
+				top = mid - 1;
+			} else if (target == arr[mid]) {
+				return mid;
+			} else {
+				low = mid + 1;
 			}
 		}
-		return -1;
+		return -1;	
 	}
+
 }

b92d1c25873fdaf911be6b99a940b9da99805b6a
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-08-31 21:35:14 -0700

    fixed bug with Binary Recursive Search

--- BinaryRecursiveSearch.java
+++ BinaryRecursiveSearch.java
@@ -3,18 +3,20 @@ public class BinaryRecursiveSearch implements Practice2Search {
 		return "Binary Recursive Search";
 	}
 	public int search(int[] arr, int target) {
-		return search(arr, target, 0, arr.length);	
+		return search(arr, target, 0, arr.length - 1);	
 	}
 	public int search(int[] arr, int target, int low, int top) {
 		if (low > top) {
 			return -1;
 		}
-		int mid = top - low / 2;
+		int mid = (top + low) / 2;
+		//System.out.printf("Low: %d\nHigh: %d\nMid: %d\nTarget: %d\n", low, top, mid, target);
 		if (target < arr[mid]) {
 			return search(arr, target, low, mid - 1);
 		} else if (target == arr[mid]) {
 			return mid;
-		} 
-		return search(arr, target, mid + 1, top);
+		} else { 
+			return search(arr, target, mid + 1, top);
+		}
 	}
 }
